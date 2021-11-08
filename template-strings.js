b1f18edeee1757822d12e2015a4f1b6047626317
Sara Barros <sarasgnt@gmail.com>
2021-11-07 23:40:08 -0300

    template literal

---
+++ template-strings.js
@@ -0,0 +1,7 @@
+const myName = "Sara";
+const age = 2021-2001;
+const birthPlace = "Salvador";
+
+const resume = `My name is ${myName}, I'm ${age} years old and my place of birth is ${birthPlace}.`;
+
+console.log(resume)
